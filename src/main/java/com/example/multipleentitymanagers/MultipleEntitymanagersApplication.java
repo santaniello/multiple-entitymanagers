6f917cf5d2c85315d3a8590b4cd89552ca4d40a8
Felipe Santaniello <you@example.com>
2020-09-27 12:31:00 -0300

    Initial Commit

---
+++ src/main/java/com/example/multipleentitymanagers/MultipleEntitymanagersApplication.java
@@ -0,0 +1,13 @@
+package com.example.multipleentitymanagers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleEntitymanagersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleEntitymanagersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multipleentitymanagers/MultipleEntitymanagersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multipleentitymanagers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleEntitymanagersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
